50c8c83c80e88deb58ee8ac55175e9a4fcaf20f4
Sean Gordon <sean_aldon@hotmail.co.uk>
2011-09-25 20:42:11 +0100

    A whole heap of shit (note to self: commit more often)

--- rakAntiGrief1/Properties.cs
+++ rakAntiGrief1/Properties.cs
@@ -14,28 +14,33 @@ namespace rakAntiGrief
         private const bool DEFAULT_BLOCK_EXPLOSIVES = true; //Block explosives from being used
         private const int DEFAULT_EXTENDED_REACH_RANGE = 6; //Max distance to edit tile/door/sign
         private const bool DEFAULT_SPAM_EXPLOSIVES_KICK = true; //Kick user if they spam explosives (using a modded client)
-		private const bool DEFAULT_BLOCK_LAVA_FLOW = true;
-		private const bool DEFAULT_BLOCK_WATER_FLOW = true;
-        
+        private const bool DEFAULT_BLOCK_LAVA_FLOW = true;
+        private const bool DEFAULT_BLOCK_WATER_FLOW = true;
+        private const bool DEFAULT_BLOCK_CHAT_SPAM = true;
+        private const bool DEFAULT_BLOCK_MOVEMENT_NOCLIP = true;
+        private const bool DEFAULT_BLOCK_MOVEMENT_TELEPORT = true;
+        private const int DEFAULT_BLOCK_MOVEMENT_TELEPORT_DISTANCE = 160;
+        private const bool DEFAULT_SPAWN_PROTECTION = true;
+        private const int DEFAULT_SPAWN_PROTECTION_TYPE = 0;
+        private const int DEFAULT_SPAWN_PROTECTION_RANGE = 25;
+
         private const String EXTENDED_REACH_DOOR = "ExtendedReachDoor";
         private const String EXTENDED_REACH = "ExtendedReach";
         private const String EXTENDED_REACH_SIGN = "ExtendedReachSign";
         private const String BLOCK_EXPLOSIVES = "BlockExplosives";
         private const String EXTENDED_REACH_RANGE = "ExtendedReachRange";
         private const String SPAM_EXPLOSIVES_KICK = "SpamExplosivesKick";
-		private const String BLOCK_LAVA_FLOW = "BlockLavaFlow";
-		private const String BLOCK_WATER_FLOW = "BlockWaterFlow";
+        private const String BLOCK_LAVA_FLOW = "BlockLavaFlow";
+        private const String BLOCK_WATER_FLOW = "BlockWaterFlow";
+        private const String BLOCK_MOVEMENT_NOCLIP = "BlockMovementNoclip";
+        private const String BLOCK_MOVEMENT_TELEPORT = "BlockMovementTeleport";
+        private const String BLOCK_MOVEMENT_TELEPORT_DISTANCE = "BlockMovementTeleportDistance";
+        private const String BLOCK_CHAT_SPAM = "BlockChatSpam";
+        private const String SPAWN_PROTECTION = "SpawnProtection";
+        private const String SPAWN_PROTECTION_TYPE = "SpawnProtectionType";
+        private const String SPAWN_PROTECTION_RANGE = "SpawnProtectionRange";
 
         public Properties(String propertiesPath) : base(propertiesPath) { }
-
-        public void pushData()
-        {
-            object temp = ExtendedReachDoor;
-            temp = ExtendedReach;
-            temp = ExtendedReachSign;
-            temp = BlockExplosives;
-            temp = ExtendedReachRange;
-        }
 
         public bool ExtendedReachDoor
         {
@@ -77,20 +82,76 @@ namespace rakAntiGrief
             }
         }
 
-		public bool BlockLavaFlow
-		{
-			get
-			{
-				return getValue(BLOCK_LAVA_FLOW, DEFAULT_BLOCK_LAVA_FLOW);
-			}
-		}
+        public bool BlockLavaFlow
+        {
+            get
+            {
+                return getValue(BLOCK_LAVA_FLOW, DEFAULT_BLOCK_LAVA_FLOW);
+            }
+        }
 
-		public bool BlockWaterFlow
-		{
-			get
-			{
-				return getValue(BLOCK_WATER_FLOW, DEFAULT_BLOCK_WATER_FLOW);
-			}
-		}
+        public bool BlockWaterFlow
+        {
+            get
+            {
+                return getValue(BLOCK_WATER_FLOW, DEFAULT_BLOCK_WATER_FLOW);
+            }
+        }
+
+        public bool BlockChatSpam
+        {
+            get
+            {
+                return getValue(BLOCK_CHAT_SPAM, DEFAULT_BLOCK_CHAT_SPAM);
+            }
+        }
+
+        public bool BlockMovementNoclip
+        {
+            get
+            {
+                return getValue(BLOCK_MOVEMENT_NOCLIP, DEFAULT_BLOCK_MOVEMENT_NOCLIP);
+            }
+        }
+
+        public bool BlockMovementTeleport
+        {
+            get
+            {
+                return getValue(BLOCK_MOVEMENT_TELEPORT, DEFAULT_BLOCK_MOVEMENT_TELEPORT);
+            }
+        }
+
+        public int BlockMovementTeleportDistance
+        {
+            get
+            {
+                return getValue(BLOCK_MOVEMENT_TELEPORT_DISTANCE, DEFAULT_BLOCK_MOVEMENT_TELEPORT_DISTANCE) * 16;
+            }
+        }
+
+        public bool SpawnProtection
+        {
+            get
+            {
+                return getValue(SPAWN_PROTECTION, DEFAULT_SPAWN_PROTECTION);
+            }
+        }
+
+        public int SpawnProtectionType
+        {
+            get
+            {
+                return getValue(SPAWN_PROTECTION_TYPE, DEFAULT_SPAWN_PROTECTION_TYPE);
+            }
+        }
+
+        public int SpawnProtectionRange
+        {
+            get
+            {
+                return getValue(SPAWN_PROTECTION_RANGE, DEFAULT_SPAWN_PROTECTION_RANGE);
+            }
+        }
     }
 }
--- rakAntiGrief1/rakAntiGrief.cs
+++ rakAntiGrief1/rakAntiGrief.cs
@@ -29,18 +29,26 @@ namespace rakAntiGrief
         public int configExtendedReachRange = 0;
         public bool configBlockLavaFlow = false;
         public bool configBlockWaterFlow = false;
+        public bool configBlockChatSpam = false;
+        public bool configBlockMovementNoclip = false;
+        public bool configBlockMovementTeleport = false;
+        public int configBlockMovementTeleportDistance = 0;
+        public bool configSpawnProtection = false;
+        public int configSpawnProtectionType = 0;
+        public int configSpawnProtectionRange = 0;
         public bool isEnabled = false;
 
         static HashSet<PlayerState> states;
         static System.Threading.Timer timer;
+        static System.Threading.Timer timer2;
 
         public override void Load()
         {
             Name = "rakAntiGrief";
             Description = "Attempts to stop common griefing attempts";
             Author = "rakiru";
-            Version = "0.2.5";
-            TDSMBuild = 29; //Current Release - Working
+            Version = "0.4.44";
+            TDSMBuild = 31; //Current Release - Working
 
             string pluginFolder = Statics.PluginPath + Path.DirectorySeparatorChar + Name;
             //Create folder if it doesn't exist
@@ -58,11 +66,20 @@ namespace rakAntiGrief
             configExtendedReachRange = properties.ExtendedReachRange;
             configBlockLavaFlow = properties.BlockLavaFlow;
             configBlockWaterFlow = properties.BlockWaterFlow;
+            configBlockChatSpam = properties.BlockChatSpam;
+            configBlockMovementNoclip = properties.BlockMovementNoclip;
+            configBlockMovementTeleport = properties.BlockMovementTeleport;
+            configBlockMovementTeleportDistance = properties.BlockMovementTeleportDistance;
+            configSpawnProtection = properties.SpawnProtection;
+            configSpawnProtectionType = properties.SpawnProtectionType;
+            configSpawnProtectionRange = properties.SpawnProtectionRange;
             properties.Save();
 
             states = new HashSet<PlayerState>();
-            timer = new System.Threading.Timer(ExpireProjectiles);
+            timer = new System.Threading.Timer(RepeatingTask);
             timer.Change(1000, 1000);
+            timer2 = new System.Threading.Timer(RepeatingTask2);
+            timer2.Change(10000, 10000);
         }
 
         public override void Enable()
@@ -75,6 +92,9 @@ namespace rakAntiGrief
             this.registerHook(Hooks.PLAYER_PROJECTILE);
             this.registerHook(Hooks.DOOR_STATECHANGE);
             this.registerHook(Hooks.PLAYER_FLOWLIQUID);
+            this.registerHook(Hooks.PLAYER_CHAT);
+            this.registerHook(Hooks.PLAYER_TELEPORT);
+            this.registerHook(Hooks.PLAYER_MOVE);
         }
 
         public override void Disable()
@@ -110,9 +130,15 @@ namespace rakAntiGrief
                 player.sendMessage("You are not allowed to use water on this server.", 255, 255, 0, 0);
                 return;
             }
-            else if (x < 0 || y < 0 || x > Main.maxTilesX || y > Main.maxTilesY || (Math.Sqrt(Math.Pow((player.Location.X / 16 - x), 2) + Math.Pow((player.Location.Y / 16 - y), 2)) > configExtendedReachRange))
+            else if (inSpawn(x, y))
             {
-                ProgramLog.Debug.Log("[" + base.Name + "]: Cancelled out of reach {1} flow by {0}", player.Name ?? player.whoAmi.ToString(), Event.Lava ? "lava" : "water");
+                Event.Cancelled = true;
+                player.sendMessage("You are not allowed to edit spawn on this server.", 255, 255, 0, 0);
+                return;
+            }
+            else if (x < 0 || y < 0 || x > Main.maxTilesX || y > Main.maxTilesY || distance(player.Location.X, player.Location.Y, x, y) > configExtendedReachRange)
+            {
+                //ProgramLog.Debug.Log("[" + base.Name + "]: Cancelled out of reach {1} flow by {0}.", player.Name ?? player.whoAmi.ToString(), Event.Lava ? "lava" : "water");
                 Event.Cancelled = true;
                 return;
             }
@@ -127,14 +153,22 @@ namespace rakAntiGrief
             else
             {
                 Player player = Event.Sender as Player;
-                if (player == null) return;
 
-                if (player.Op) return;
+                if (player == null || player.Op == true)
+                {
+                    return;
+                }
 
                 int x = (int)Event.Position.X;
                 int y = (int)Event.Position.Y;
 
-                if (x < 0 || y < 0 || x > Main.maxTilesX || y > Main.maxTilesY || (Math.Sqrt(Math.Pow((player.Location.X / 16 - Event.Position.X), 2) + Math.Pow((player.Location.Y / 16 - Event.Position.Y), 2)) > configExtendedReachRange))
+                if (inSpawn(x, y))
+                {
+                    Event.Cancelled = true;
+                    player.sendMessage("You are not allowed to edit spawn on this server.", 255, 255, 0, 0);
+                    return;
+                }
+                else if (x < 0 || y < 0 || x > Main.maxTilesX || y > Main.maxTilesY || distance(player.Location.X, player.Location.Y, x, y) > configExtendedReachRange)
                 {
                     Event.Cancelled = true;
                     return;
@@ -150,17 +184,27 @@ namespace rakAntiGrief
             }
             else
             {
-                Player Player = Event.Sender as Player;
+                Player player = Event.Sender as Player;
 
-                if (Player != null && Player.AuthenticatedAs == null)
+                if (player == null || player.Op == true)
                 {
-                    Event.Cancelled = true;
+                    return;
                 }
-                if (Player != null && Math.Sqrt(Math.Pow((Player.Location.X / 16 - Event.Sign.x), 2) + Math.Pow((Player.Location.Y / 16 - Event.Sign.y), 2)) > configExtendedReachRange)
+
+                int x = (int)Event.Sign.x;
+                int y = (int)Event.Sign.y;
+
+                if (inSpawn(x, y))
                 {
                     Event.Cancelled = true;
-                    Player.sendMessage("You are too far away to edit that sign.", 255, 255, 0, 0);
-                    ProgramLog.Admin.Log("[" + base.Name + "]: Cancelled Sign Edit of Player: " + Player.Name);
+                    player.sendMessage("You are not allowed to edit spawn on this server.", 255, 255, 0, 0);
+                    return;
+                }
+                else if (distance(player.Location.X, player.Location.Y, x, y) > configExtendedReachRange)
+                {
+                    Event.Cancelled = true;
+                    player.sendMessage("You are too far away to edit that sign.", 255, 255, 0, 0);
+                    ProgramLog.Admin.Log("[" + base.Name + "]: Cancelled Sign Edit of Player: " + player.Name);
                 }
             }
         }
@@ -173,26 +217,19 @@ namespace rakAntiGrief
             }
             else
             {
-                Player Player = Event.Sender as Player;
-                if (Player != null && Math.Sqrt(Math.Pow((Player.Location.X / 16 - Event.X), 2) + Math.Pow((Player.Location.Y / 16 - Event.Y), 2)) > configExtendedReachRange)
+                Player player = Event.Sender as Player;
+
+                if (player == null || player.Op == true)
+                {
+                    return;
+                }
+
+                if (distance(player.Location.X, player.Location.Y, Event.X, Event.Y) > configExtendedReachRange)
                 {
                     Event.Cancelled = true;
-                    Player.sendMessage("You are too far away to " + Event.Direction + " that door", 255, 255, 0, 0);
-                    ProgramLog.Admin.Log("[" + base.Name + "]: Cancelled Door Change of Player: " + Player.Name);
+                    player.sendMessage("You are too far away to " + Event.Direction + " that door", 255, 255, 0, 0);
+                    ProgramLog.Admin.Log("[" + base.Name + "]: Cancelled Door Change of Player: " + player.Name);
                 }
-            }
-        }
-
-        class PlayerState
-        {
-            public int projectiles;
-        }
-
-        public void ExpireProjectiles(object dummy)
-        {
-            foreach (var state in states)
-            {
-                state.projectiles = 0;
             }
         }
 
@@ -215,11 +252,16 @@ namespace rakAntiGrief
                     states.Add(state);
                 }
                 else
+                {
                     state = (PlayerState)player.PluginData[this];
+                }
 
                 ProjectileType type = Event.Projectile.type;
 
-                state.projectiles += 1;
+                if (!(type == ProjectileType.ORB_OF_LIGHT || type == ProjectileType.FLAMELASH || type == ProjectileType.MISSILE_MAGIC))
+                {
+                    state.projectiles += 1;
+                }
 
                 if (state.projectiles >= 9)
                 {
@@ -229,15 +271,233 @@ namespace rakAntiGrief
                     return;
                 }
 
-
-                //Program.tConsole.WriteLine("[" + base.Name + "] " + Event.Sender.Name + " Launched Projectile of type " + type + "(" + Event.Projectile.Name + ")");
-                if (player != null && (type == ProjectileType.DYNAMITE || type == ProjectileType.GRENADE || type == ProjectileType.BOMB || type == ProjectileType.BOMB_STICKY))
+                if (player != null && (type == ProjectileType.DYNAMITE || type == ProjectileType.GRENADE || type == ProjectileType.BOMB || type == ProjectileType.BOMB_STICKY || type == ProjectileType.ARROW_HELLFIRE))
                 {
                     Event.Cancelled = true;
                     player.sendMessage("You are not allowed to use explosives on this server.", 255, 255, 0, 0);
                     ProgramLog.Admin.Log("[" + base.Name + "]: Cancelled Projectile Use of Player: " + ((Player)Event.Sender).Name);
                 }
             }
+        }
+
+        public override void onPlayerChat(MessageEvent Event)
+        {
+            if (isEnabled == false || configBlockChatSpam == false)
+            {
+                return;
+            }
+            else
+            {
+                Player player = Event.Sender as Player;
+                if (player == null || player.Op) return;
+
+                PlayerState state = null;
+                if (!player.PluginData.Contains(this))
+                {
+                    state = new PlayerState();
+                    player.PluginData[this] = state;
+                    states.Add(state);
+                }
+                else
+                {
+                    state = (PlayerState)player.PluginData[this];
+                }
+
+                state.messages += 1;
+
+                if (state.messages >= 9)
+                {
+                    Event.Cancelled = true;
+                    state.messages -= 9;
+                    ProgramLog.Admin.Log("[" + base.Name + "]: Stopped chat spam from {1}.", player.Name ?? "<null>");
+                    player.Kick("Do not spam chat.");
+                    return;
+                }
+            }
+        }
+
+        public override void onPlayerTeleport(PlayerTeleportEvent Event)
+        {
+            if (isEnabled == false || configBlockMovementTeleport == false)
+            {
+                return;
+            }
+            else
+            {
+                Player player = Event.Sender as Player;
+                if (player == null || player.Op) return;
+
+                PlayerState state = null;
+                if (!player.PluginData.Contains(this))
+                {
+                    state = new PlayerState();
+                    player.PluginData[this] = state;
+                    states.Add(state);
+                }
+                else
+                {
+                    state = (PlayerState)player.PluginData[this];
+                }
+                state.teleported = DateTime.UtcNow;
+            }
+        }
+
+        public override void onPlayerMove(PlayerMoveEvent Event)
+        {
+            Player player = Event.Sender as Player;
+            if (player == null || player.Op) return;
+
+            PlayerState state = null;
+            if (!player.PluginData.Contains(this))
+            {
+                state = new PlayerState();
+                player.PluginData[this] = state;
+                states.Add(state);
+            }
+            else
+            {
+                state = (PlayerState)player.PluginData[this];
+            }
+            state.movePackets += 1;
+
+            if (state.movePackets >= 29)
+            {
+                if (++state.movePacketsViolations > 9)
+                {
+                    ProgramLog.Admin.Log("[" + base.Name + "]: Stopped position update spam from {0}.", player.Name ?? "<null>");
+                    player.Kick("Do not spam packets.");
+                    return;
+                }
+            }
+
+            var tile = Main.tile.At((int)Event.Location.X / 16, (int)Event.Location.Y / 16);
+            if (tile.Active && Main.tileSolid[tile.Type] && tile.Type != 19 && ++state.noclips > 3)
+            {
+                ProgramLog.Admin.Log("[" + base.Name + "]: Noclip detected for {1}.", player.Name ?? "<null>");
+                player.Kick("Noclip detected.");
+                return;
+            }
+            return;
+        }
+
+        class PlayerState
+        {
+            public int projectiles;
+            public int messages;
+            public DateTime teleported;
+            public int movePackets;
+            public int movementViolations;
+            public int movePacketsViolations;
+            public int noclips;
+            public float x;
+            public float y;
+
+            public PlayerState()
+            {
+                this.x = Main.spawnTileX * 16;
+                this.y = Main.spawnTileY * 16;
+                this.teleported = DateTime.UtcNow;
+            }
+        }
+
+        public void RepeatingTask(object dummy)
+        {
+            DateTime startTime = DateTime.UtcNow;
+            foreach (var state in states)
+            {
+                state.projectiles = 0;
+                state.messages = 0;
+                state.movePackets = 0;
+                state.noclips = 0;
+            }
+
+            foreach (Player player in Main.players)
+            {
+                if (!player.Active) continue;
+                int x = (int)player.Location.X;
+                int y = (int)player.Location.Y;
+                PlayerState state = null;
+                if (!player.PluginData.Contains(this))
+                {
+                    state = new PlayerState();
+                    player.PluginData[this] = state;
+                    states.Add(state);
+                }
+                else
+                {
+                    state = (PlayerState)player.PluginData[this];
+                }
+                double dist = Math.Sqrt(Math.Pow((x - state.x), 2) + Math.Pow((y - state.y), 2));
+                if (dist > configBlockMovementTeleportDistance)
+                {
+                    double time = DateTime.UtcNow.Subtract(state.teleported).TotalMilliseconds;
+                    float distSpawn = distance(x, y, player.SpawnX / 16, player.SpawnY / 16);
+                    if (time > 1000 && distSpawn > 16)
+                    {
+                        ProgramLog.Admin.Log("[" + base.Name + "]: Movement hacking (or high lag) detected for {0}.", player.Name ?? "<null>");
+                        //ProgramLog.Debug.Log("[" + base.Name + "]: distance: {0}   confixDistance: {1}   timeDif: {2}   distanceFromSpawn: {3}    {4}, {5}, {6}, {7}", dist, configBlockMovementTeleportDistance, time, distSpawn, x, y, player.SpawnX, player.SpawnY);
+                        if (++state.movementViolations > 3)
+                        {
+                            player.Kick("Movement hacking (or high lag) detected.");
+                            continue;
+                        }
+                    }
+                }
+                //ProgramLog.Debug.Log("[" + base.Name + "]: Checked {0} at {1},{2} from {3},{4}!", player.Name, x, y, state.x, state.y);
+                state.x = x;
+                state.y = y;
+            }
+            //ProgramLog.Debug.Log("[" + base.Name + "]: Time taken on RepeatingTask {0}ms", DateTime.UtcNow.Subtract(startTime).Milliseconds);
+        }
+
+        public void RepeatingTask2(object dummy)
+        {
+            DateTime startTime = DateTime.UtcNow;
+            foreach (var state in states)
+            {
+                state.movementViolations = 0;
+                state.movePacketsViolations = 0;
+            }
+            //ProgramLog.Debug.Log("[" + base.Name + "]: Time taken on RepeatingTask {0}ms", DateTime.UtcNow.Subtract(startTime).Milliseconds);
+        }
+
+        private bool inSpawn(int playerX, int playerY)
+        {
+            if (!configSpawnProtection)
+            {
+                return false;
+            }
+
+            int spawnX = this.Server.World.SpawnX;
+            int spawnY = this.Server.World.SpawnY;
+
+            switch (configSpawnProtectionType)
+            {
+                case 0: //Vertical strip spawn protection
+                    if (playerX > (spawnX - configSpawnProtectionRange) && playerX < (spawnX + configSpawnProtectionRange))
+                    {
+                        return true;
+                    }
+                    break;
+                case 1: //Square spawn protection
+                    if (playerX > (spawnX - configSpawnProtectionRange) && playerX < (spawnX + configSpawnProtectionRange) && playerY > (spawnY - configSpawnProtectionRange) && playerY < (spawnY + configSpawnProtectionRange))
+                    {
+                        return true;
+                    }
+                    break;
+                case 2: //Circle spawn protection
+                    if (distance(playerX, spawnX, playerY, spawnY) < configSpawnProtectionRange)
+                    {
+                        return true;
+                    }
+                    break;
+            }
+            return false;
+        }
+
+        public float distance(float playerX, float playerY, float tileX, float tileY)
+        {
+            return (float)Math.Sqrt(Math.Pow((playerX / 16 - tileX), 2) + Math.Pow((playerY / 16 - tileY), 2));
         }
 
         private static void CreateDirectory(string dirPath)
